fceb5777700b02a611a0e4b90b9795d569ea66a5
unknown <frogtown.spiral@gmail.com>
2026-03-24 04:00:45 -0400

    dev: frogtown website v1.

--- config/emailjs-config.js
+++
@@ -1,7 +0,0 @@
-// 🔑 Replace these with YOUR values from EmailJS
-
-const EMAILJS_CONFIG = {
-  PUBLIC_KEY: "xWkQZck2SG6kjqK6G",
-  SERVICE_ID: "Frogtown_Contact_83828",
-  TEMPLATE_ID: "template_hf79hke"
-};
--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-// Initialize EmailJS
-(function () {
-  emailjs.init(EMAILJS_CONFIG.PUBLIC_KEY);
-})();
+emailjs.init("YOUR_PUBLIC_KEY");
 
-document.getElementById("contact-form").addEventListener("submit", function (e) {
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  emailjs.sendForm(
-    EMAILJS_CONFIG.SERVICE_ID,
-    EMAILJS_CONFIG.TEMPLATE_ID,
-    this
-  )
-  .then(() => {
-    alert("Request sent successfully!");
-    this.reset();
-  })
-  .catch((error) => {
-    alert("Failed to send. Try again.");
-    console.error(error);
-  });
+  const button = form.querySelector("button");
+  button.innerText = "Sending...";
+  button.disabled = true;
+
+  emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
+    .then(() => {
+      button.innerText = "Sent!";
+      form.reset();
+    })
+    .catch(() => {
+      button.innerText = "Error - Try Again";
+      button.disabled = false;
+    });
 });
